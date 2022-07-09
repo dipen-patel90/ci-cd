cde51258df24abb21689083ce3f7a5e307c71fff
Dipen Patel <dipen.ptl1@gmail.com>
2022-07-09 16:52:13 +0530

    Updated workflow to generate signed bundle

--- app/src/main/java/com/ci/cd/MainActivity.kt
+++ app/src/main/java/com/ci/cd/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.ci.cd
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<TextView>(R.id.local_properties).text = BuildConfig.PROPERTIES_URL
     }
 }

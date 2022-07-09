993047e90c3c5863dd5562e430e4963746ef2533
Dipen Patel <dipen.ptl1@gmail.com>
2022-07-09 14:16:23 +0530

    accessing PROPERTIES_URL in workflow

--- app/src/main/java/com/ci/cd/MainActivity.kt
+++ app/src/main/java/com/ci/cd/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        findViewById<TextView>(R.id.local_properties).text = BuildConfig.LOCAL_PROPERTIES
+        findViewById<TextView>(R.id.local_properties).text = BuildConfig.PROPERTIES_URL
     }
 }

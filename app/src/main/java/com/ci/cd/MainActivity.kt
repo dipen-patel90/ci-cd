e982dbb32686fb2c7934710c2b5a5a3a4c9120fd
Dipen Patel <dipen.ptl1@gmail.com>
2022-07-09 11:22:48 +0530

    Displaying value from local-properties

--- app/src/main/java/com/ci/cd/MainActivity.kt
+++ app/src/main/java/com/ci/cd/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.ci.cd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.local_properties).text = BuildConfig.LOCAL_PROPERTIES
     }
 }
